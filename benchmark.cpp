f0b081e8a481c6dffca8d5b68395df4702e6ae75
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2018-05-18 13:41:25 +0200

    Print benchmarks as table

--- benchmark.cpp
+++ benchmark.cpp
@@ -2,82 +2,101 @@
 
 using namespace std;
 
-template <typename T>
-void benchmarkColumnLayout(uint64_t length) {
-    vector<T> memoryWaster(length, 0);
-    uint64_t counter = 0;
+class Benchmarker {
+private:
+    int storeType;
+    int dataType;
+    int columnSize;
 
-    clock_t begin = clock();
-    for (auto it = memoryWaster.begin(); it != memoryWaster.end(); ++it) {
-        if (*it == 0) {
-            counter++;
+private:
+    static void printHeader() {
+        cout << "store_type,data_type,column_size,bandwidth\n";
+    }
+
+    void printAttributes() {
+        if (storeType == 0) {
+            cout << "column_store,";
+        } else {
+            cout << "row_store,";
+        }
+        cout << "uint" << dataType << "_t," << columnSize << ",";
+    }
+
+    template <typename T>
+    void benchmarkColumnLayout() {
+        vector<T> memoryWaster(columnSize, 0);
+        uint64_t counter = 0;
+
+        auto startTime = chrono::high_resolution_clock::now();
+        for (auto it = memoryWaster.begin(); it != memoryWaster.end(); ++it) {
+            if (*it == 0) {
+                counter++;
+            }
+        }
+        auto endTime = chrono::high_resolution_clock::now();
+
+        uint64_t nanoSeconds = chrono::duration_cast<chrono::nanoseconds>(endTime - startTime).count();
+        cout << sizeof(T) * columnSize / nanoSeconds << '\n';
+        assert(counter == columnSize);
+    }
+
+    template <typename T>
+    void benchmarkRowLayout() {
+        vector<vector<T>> memoryWaster(columnSize, vector<T>(100, 0));
+        uint64_t counter = 0;
+
+        clock_t begin = clock();
+        for (auto it = memoryWaster.begin(); it != memoryWaster.end(); ++it) {
+            if (it->at(0) == 0) {
+                counter++;
+            }
+        }
+        clock_t end = clock();
+        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        cout << sizeof(T) * columnSize / elapsed_secs / 1e9 << '\n';
+        assert(counter == columnSize);
+    }
+
+    template <typename T>
+    void benchmark() {
+        if (storeType == 0) {
+            return benchmarkColumnLayout<T>();
+        } else {
+            return benchmarkRowLayout<T>();
         }
     }
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout << sizeof(T) * length / elapsed_secs / 1e9 << " GB/s" << endl;
-    cout << elapsed_secs << " s" << endl;
-    cout << counter << " should equal " << length << endl;
-    assert(counter == length);
-}
 
-template <typename T>
-void benchmarkRowLayout(uint64_t length) {
-    vector<vector<T>> memoryWaster(length, vector<T>(100, 0));
-    uint64_t counter = 0;
-
-    clock_t begin = clock();
-    for (auto it = memoryWaster.begin(); it != memoryWaster.end(); ++it) {
-        if (it->at(0) == 0) {
-            counter++;
+public:
+    void runAllBenchmarks() {
+        printHeader();
+        for (storeType = 0; storeType <= 1; storeType++) {
+            for (dataType = 8; dataType <= 64; dataType *= 2) {
+                for (columnSize = 1; columnSize <= 1e7; columnSize *= 10) {
+                    printAttributes();
+                    switch (dataType) {
+                        case 8:
+                            benchmark<uint8_t>();
+                            break;
+                        case 16:
+                            benchmark<uint16_t>();
+                            break;
+                        case 32:
+                            benchmark<uint32_t>();
+                            break;
+                        case 64:
+                            benchmark<uint64_t>();
+                            break;
+                        default:
+                            cerr << "Invalid value" << endl;
+                    }
+                }
+            }
         }
+        cout.flush();
     }
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    cout << sizeof(T) * length / elapsed_secs / 1e9 << " GB/s" << endl;
-    cout << elapsed_secs << endl;
-    cout << counter << " should equal " << length << endl;
-    assert(counter == length);
-}
-
-template <typename T>
-void benchmark(uint64_t length, bool useColumnLayout) {
-    if (useColumnLayout) {
-        return benchmarkColumnLayout<T>(length);
-    } else {
-        return benchmarkRowLayout<T>(length);
-    }
-}
+};
 
 int main(int argc, char *argv[]) {
-    if (argc < 2 || argc > 3) {
-        cerr << "Usage: ./program length int_type [row]" << endl;
-        cerr << "Example: ./program 1000000 64 1" << endl;
-        return 1;
-    }
-    const uint64_t length = atoi(argv[0]);
-    const int int_type = atoi(argv[1]);
-    bool useColumnLayout = true;
-
-    if (argc == 3) {
-        useColumnLayout = atoi(argv[2]) == 1;
-    }
-
-    switch(int_type) {
-        case 8:
-            benchmark<uint8_t>(length, useColumnLayout);
-            break;
-        case 16:
-            benchmark<uint16_t>(length, useColumnLayout);
-            break;
-        case 32:
-            benchmark<uint32_t>(length, useColumnLayout);
-            break;
-        case 64:
-            benchmark<uint64_t>(length, useColumnLayout);
-            break;
-        default:
-            cerr << "Unvalid value" << endl;
-    }
-    vector<uint64_t> memoryWaster;
+    Benchmarker *benchmarker = new Benchmarker;
+    benchmarker->runAllBenchmarks();
 }
